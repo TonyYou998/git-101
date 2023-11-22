fa0b5eb6d470d0d3c19000e815c55c34bc5ec8a4
vuu tan <tony_you@SuperMacbook.local>
2023-11-22 14:20:28 +0700

    done AB-03

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,6 @@ public class MyClass {
     public static void main (String args[]){
         System.out.println("hello world");
         System.out.println("asdasda");
+        System.out.println("AB-03");
     }
 }
